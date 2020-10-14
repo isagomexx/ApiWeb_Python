3fb31c2c3284b1b4c6f6b792a77abf8a8a5da805
Maria Isabel <isagomex1@gmail.com>
2020-10-14 17:23:51 -0500

    cambios-isa

---
+++ ApiWeb/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\xime-\onedrive\documentos\apiweb_python\apiweb\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

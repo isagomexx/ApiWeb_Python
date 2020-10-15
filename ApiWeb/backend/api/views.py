d18d618ea0a53992af11f034ec3cc7f47d726622
Alexis <amurillo82943@misena.edu.co>
2020-10-14 23:07:51 -0500

    Cambios (views)

--- ApiWeb/backend/api/views.py
+++ ApiWeb/backend/api/views.py
@@ -3,21 +3,55 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
+from .models import Paciente, Doctor, GrupoFamilia, Examen, Cita, Incapacidad, Historial, FormulaMedica, Orden
 
 @api_view(['POST'])
-def autenticacion(request):
-
-    username = request.data.get('usuario')
-    if not username:
-        return None
+def registro(request):
+    tipo_doc = request.data.get('tipo_documento')
+    num_id = request.data.get('numero_identificacion')
+    nombre = request.data.get('nombres')
+    apellidop = request.data.get('apellido_paterno')
+    apellidom = request.data.get('apellido_materno')
+    tel = request.data.get('telefono')
+    direc = request.data.get('direccion')
+    email = request.data.get('correo')
+    espec = request.data.get('especialidad')
+    usu = request.data.get('usuario')
+    password = request.data.get('contrasena')
 
     try:
-        user ='ISABE'
-        return Response(user)
+        doctor = Doctor()
+        doctor.tipo_documento = tipo_doc
+        doctor.numero_identificacion = num_id
+        doctor.nombres = nombre
+        doctor.apellido_paterno = apellidop
+        doctor.apellido_materno = apellidom
+        doctor.telefono = tel
+        doctor.direccion = direc
+        doctor.correo = email
+        doctor.especialidad = espec
+        doctor.usuario = usu
+        doctor.contrasena = password
+        doctor.save()
+
+        insertar = {'tipo de documento': tipo_doc,
+                    'Numero de identificacion':num_id,
+                    'Nombres':nombre,
+                    'Apelido paterno':apellidop,
+                    'Apelido materno':apellidom,
+                    'Telefono':tel,
+                    'Direccion':direc,
+                    'Correo':email,
+                    'Especialidad':espec,
+                    'Usuario':usu,
+                    'Contraseña':password,
+                    'insertado': '200 Ok',
+                    'error': 0}
+
+    except Exception as e:
+        error = 'No se ha podido insertar el registro.  ' + str(e)
+        insertar = {'doctor': None,
+                    'insertado': False,
+                    'error': error}
     
-    except Exception as e :
-             raise AuthenticationFailed('No such user')
-
-             return Response(e)
-
-# Create your views here.
+    return Response(insertar)
---
+++ ApiWeb/front/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { IniciarComponent } from './components/iniciar/iniciar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RegistroComponent } from './components/registro/registro.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IniciarComponent,
+    NavbarComponent,
+    RegistroComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
